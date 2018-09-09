e1af87ee568010b66272bf603e8f9276cf66f9c1
shao yuan <shaoyuan_chewchia@college.harvard.edu>
2018-09-09 00:31:29 -0400

    PA-1 PA-2 Successfully connected to SPF and got readings

--- app/src/main/java/shaoyuan/spiro/ui/HomeFragment.java
+++ app/src/main/java/shaoyuan/spiro/ui/HomeFragment.java
@@ -3,6 +3,7 @@ package shaoyuan.spiro.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +25,12 @@ public class HomeFragment extends Fragment {
         final Button calibrateButton = v.findViewById(R.id.calibrateButton);
         calibrateButton.setOnClickListener(createCalibrateButtonListener());
 
+        final Button startMeasureButton = v.findViewById(R.id.startMeasureButton);
+        startMeasureButton.setOnClickListener(createStartButtonListener());
+
+        final Button stopMeasureButton = v.findViewById(R.id.stopMeasureButton);
+        stopMeasureButton.setOnClickListener(createStopButtonListener());
+
         return v;
     }
 
@@ -38,6 +45,28 @@ public class HomeFragment extends Fragment {
                 });
             }
         };
+    }
+
+    private View.OnClickListener createStartButtonListener() {
+        return new View.OnClickListener() {
+            public void onClick(View v) {
+                MicrophoneSignalProcess.getInstance().startAnalyze(new SignalProcess.OnPeakFound() {
+                    @Override
+                    public void onResult(int peakFlowRate) {
+                        Log.d("SPF-Lib","Peak Flow Rate: " + peakFlowRate);
+                    }
+                });
+            }
+        };
+    }
+
+    private View.OnClickListener createStopButtonListener() {
+        return new View.OnClickListener() {
+            public void onClick(View v) {
+                    MicrophoneSignalProcess.getInstance().stopAnalyze();
+                    MicrophoneSignalProcess.getInstance().close();
+                }
+            };
     }
 
 }
